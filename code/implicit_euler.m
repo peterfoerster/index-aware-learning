505e2863d6abde4c0c5c48c5a6702691ba9b9a94
peter <peterfrankfoerster@gmail.com>
2025-09-10 11:54:04 +0200

    peter

--- code/implicit_euler.m
+++ code/implicit_euler.m
@@ -1,14 +1,14 @@
-%   [x, t] = implicit_euler (T_0, T, Deltat, M, K, f, x_0)
+%   [x, t] = implicit_euler (t_0, T, Deltat, M, K, f, x_0)
 %
 % Computes the numerical solution of an initial value problem of the form
-%      M(x) x' + K(x) x = f(t) on [T_0,T]
+%      M(x) x' + K(x) x = f(t) on [t_0, T]
 %               x(0) = x_0
 % using the implicit Euler method.
 %
 % INPUT:
-%       - T_0, T, Deltat: time interval [T_0,T] and timestep
-%       - M: @(x) nonlinear matrix
-%       - K: @(x) nonlinear matrix
+%       - t_0, T, Deltat: time interval [t_0, T] and timestep
+%       - M: @(x) nonlinear (mass) matrix
+%       - K: @(x) nonlinear (stiffness) matrix
 %       - f: @(t) time dependent rhs
 %       - x_0: initial condition
 %
@@ -16,20 +16,21 @@
 %       - x: [N_x, N_t] solution vector at the discrete points in time
 %       - t: [1, N_t] vector containing the discrete points in time
 
-function [x, t] = implicit_euler (T_0, T, Deltat, M, K, f, x_0)
-    N_x = length(x_0);
-    N_t = round((T - T_0) / Deltat);
-    x   = NaN(N_x, N_t+1);
+function [x, t] = implicit_euler (t_0, T, Deltat, M, K, f, x_0)
+    N_x    = length(x_0);
+    N_t    = ceil((T - t_0) / Deltat);
+    x      = NaN(N_x, N_t+1);
     x(:,1) = x_0;
 
-    t = T_0:Deltat:(T_0 + N_t*Deltat);
+    t = t_0:Deltat:(t_0 + N_t*Deltat);
+
     for n=1:N_t
         if (~mod(n, 1000))
-            fprintf('\niteration no.: %i/%i\n', n, N_t);
+            fprintf('\niteration # %i/%i\n', n, N_t);
         end
 
         % M(x_npo) (x_npo - x_n) + Deltat K(x_npo) x_npo = Deltat f(t_npo)
-        g = @(x_npo) M(x_npo)*(x_npo - x(:,n)) + Deltat*K(x_npo)*x_npo - Deltat*f(t(n+1));
+        g = @(x_npo) M(x_npo) * (x_npo - x(:,n)) + Deltat*K(x_npo) * x_npo - Deltat*f(t(n+1));
 
         options.TolX   = 1e-16;
         options.TolFun = 1e-16;
